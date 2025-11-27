0b80353271dfd80bc8d84e960878014d2e8b9cf8
Admin <Admin@DESKTOP-3UI83V5>
2025-11-27 15:58:53 +0530

    new methods is added

--- selenium/src/test/java/practice/methods.java
+++ selenium/src/test/java/practice/methods.java
@@ -9,22 +9,22 @@ public class methods {
 		// TODO Auto-generated method stub
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://demowebshop.tricentis.com/");
-//		String x=driver.getTitle();
-//		System.out.println(x);
+		String x=driver.getTitle();
+		System.out.println(x);
 		
-//		System.out.println(driver.getCurrentUrl());
-//		System.out.println(driver.getPageSource());
-//		driver.navigate().to("https://demowebshop.tricentis.com/");
-//		Thread.sleep(1000);
-//		driver.navigate().back();
-//		Thread.sleep(1000);
-//		driver.navigate().forward();
-//		Thread.sleep(1000);
-//		driver.navigate().refresh();
-//		Thread.sleep(1000);
-//		driver.manage().window().maximize();
-//		Thread.sleep(1000);
-//		driver.close();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getPageSource());
+		driver.navigate().to("https://demowebshop.tricentis.com/");
+		Thread.sleep(1000);
+		driver.navigate().back();
+		Thread.sleep(1000);
+		driver.navigate().forward();
+		Thread.sleep(1000);
+		driver.navigate().refresh();
+		Thread.sleep(1000);
+		driver.manage().window().maximize();
+		Thread.sleep(1000);
+		driver.close();
 	}
 	
 
